842a6839b927cda4dd59d4c9a29bdd862eb11058
Abhisheksinghpawar <abhisheksinghpawar17@gmail.com>
2024-09-07 02:39:30 -0500

    First Commit

---
+++ jokeAPINotification.py
@@ -0,0 +1,30 @@
+'''https://api-ninjas.com/profile'''
+'''UN - abhisheksinghpawar17@gmail.com'''
+'''PWD - Metroplaza17'''
+'''DATE CREATED 6 SEPT 2024 - TESTED ON VSCODE'''
+
+import time
+from plyer import notification
+import requests
+import random
+
+def getFact(data):
+
+    api_url_facts = 'https://api.api-ninjas.com/v1/'+data+'s?'
+    
+    response = requests.get(api_url_facts, headers={'X-Api-Key': 'tW/0ZrhGUfy68npIggfjHA==OgnYI6awvCrr74eG'})
+    
+    if response.status_code == requests.codes.ok:
+        return response.json()[0].get(data)
+    else:
+        return "API call failed"
+
+while True:
+    data = random.choice(["fact","joke"])
+    notification.notify(
+        title="Here's your "+data+" !",
+        message=getFact(data),
+        app_icon = None,
+        timeout = 15
+    )
+    time.sleep(30)
